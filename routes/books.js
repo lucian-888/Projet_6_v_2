9b0a6a5104a41ccf7f1563235bd91102bfeb4220
Lucian Bistreanu <lucianbistreanu@yahoo.com>
2024-09-17 19:55:16 +0200

    removing 'auth' from router.get. I can see now all the books and one book

--- routes/books.js
+++ routes/books.js
@@ -16,9 +16,9 @@ router.use((req, res, next) => {
   });
 
 
-router.get('/', auth, booksCtrl.getAllBooks);
+router.get('/', booksCtrl.getAllBooks);
 router.post('/', auth, multer, booksCtrl.createBook);
-router.get('/:id', auth, booksCtrl.getOneBook);
+router.get('/:id', booksCtrl.getOneBook);
 router.put('/:id', auth, multer, booksCtrl.modifyBook);
 router.delete('/:id', auth, booksCtrl.deleteBook);
 
